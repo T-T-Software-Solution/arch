6dff428692c9e5e42d99518a67819da2a4086819
Au <au.perspectives@gmail.com>
2024-01-09 19:18:31 +0700

    Migrate Core.gRPC project.

---
+++ src/TTSS.Core.gRPC/gRPC/Interceptors/GrpcExceptionLogInterceptor.cs
@@ -0,0 +1,112 @@
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+using TTSS.Core.Loggings;
+
+namespace TTSS.Core.gRPC.Interceptors;
+
+/// <summary>
+/// Interceptor for logging exceptions.
+/// </summary>
+public class GrpcExceptionLogInterceptor : Interceptor
+{
+    #region Fields
+
+    private readonly IActivityFactory _activityFactory;
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GrpcExceptionLogInterceptor"/> class.
+    /// </summary>
+    /// <param name="activityFactory">The activity factory</param>
+    /// <exception cref="ArgumentNullException">The activity factory is required</exception>
+    public GrpcExceptionLogInterceptor(IActivityFactory activityFactory)
+        => _activityFactory = activityFactory ?? throw new ArgumentNullException(nameof(activityFactory));
+
+    #endregion
+
+    /// <summary>
+    /// Server-side handler for intercepting and incoming unary call.
+    /// </summary>
+    /// <typeparam name="TRequest">Request message type for this method</typeparam>
+    /// <typeparam name="TResponse">Response message type for this method</typeparam>
+    /// <param name="request">The request value of the incoming invocation</param>
+    /// <param name="context">The invocation context</param>
+    /// <param name="continuation">The next handler to invoke</param>
+    /// <returns>The response</returns>
+    public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(
+        TRequest request,
+        ServerCallContext context,
+        UnaryServerMethod<TRequest, TResponse> continuation)
+    {
+        try { return await continuation(request, context); }
+        catch (Exception e) { throw e.Handle<TRequest>(new(request, context, CreateActivity(context.Method))); }
+    }
+
+    /// <summary>
+    /// Server-side handler for intercepting client streaming call.
+    /// </summary>
+    /// <typeparam name="TRequest">Request message type for this method</typeparam>
+    /// <typeparam name="TResponse">Response message type for this method</typeparam>
+    /// <param name="requestStream">The request stream of the incoming invocation</param>
+    /// <param name="context">The invocation context</param>
+    /// <param name="continuation">The next handler to invoke</param>
+    /// <returns>The response</returns>
+    public override async Task<TResponse> ClientStreamingServerHandler<TRequest, TResponse>(
+        IAsyncStreamReader<TRequest> requestStream,
+        ServerCallContext context,
+        ClientStreamingServerMethod<TRequest, TResponse> continuation)
+    {
+        try { return await continuation(requestStream, context); }
+        catch (Exception e) { throw e.Handle<TRequest>(new(requestStream.Current, context, CreateActivity(context.Method))); }
+    }
+
+    /// <summary>
+    /// Server-side handler for intercepting server streaming call.
+    /// </summary>
+    /// <typeparam name="TRequest">Request message type for this method</typeparam>
+    /// <typeparam name="TResponse">Response message type for this method</typeparam>
+    /// <param name="request">The request value of the incoming invocation</param>
+    /// <param name="responseStream">The response stream of the incoming invocation</param>
+    /// <param name="context">The invocation context</param>
+    /// <param name="continuation">The next handler to invoke</param>
+    /// <returns>The response</returns>
+    public override async Task ServerStreamingServerHandler<TRequest, TResponse>(
+        TRequest request,
+        IServerStreamWriter<TResponse> responseStream,
+        ServerCallContext context,
+        ServerStreamingServerMethod<TRequest, TResponse> continuation)
+    {
+        try { await continuation(request, responseStream, context); }
+        catch (Exception e) { throw e.Handle<TRequest>(new(request, context, CreateActivity(context.Method))); }
+    }
+
+    /// <summary>
+    /// Server-side handler for intercepting bidirectional streaming calls.
+    /// </summary>
+    /// <typeparam name="TRequest">Request message type for this method</typeparam>
+    /// <typeparam name="TResponse">Response message type for this method</typeparam>
+    /// <param name="requestStream">The request stream of the incoming invocation</param>
+    /// <param name="responseStream">The response stream of the incoming invocation</param>
+    /// <param name="context">The invocation context</param>
+    /// <param name="continuation">The next handler to invoke</param>
+    /// <returns>The response</returns>
+    public override async Task DuplexStreamingServerHandler<TRequest, TResponse>(
+        IAsyncStreamReader<TRequest> requestStream,
+        IServerStreamWriter<TResponse> responseStream,
+        ServerCallContext context,
+        DuplexStreamingServerMethod<TRequest, TResponse> continuation)
+    {
+        try { await continuation(requestStream, responseStream, context); }
+        catch (Exception e) { throw e.Handle<TRequest>(new(requestStream.Current, context, CreateActivity(context.Method))); }
+    }
+
+    private IActivity? CreateActivity(string methodPath)
+    {
+        var categoryName = methodPath.Split("/", StringSplitOptions.RemoveEmptyEntries).FirstOrDefault() ?? methodPath;
+        var callerMethodName = Path.GetFileName(methodPath) ?? "UnknownMethod";
+        return _activityFactory.CreateActivity(categoryName, callerMethodName, System.Diagnostics.ActivityKind.Server);
+    }
+}
---
+++ src/TTSS.Core.gRPC/gRPC/Interceptors/GrpcExceptionParameter.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using System.Text.Json;
+using TTSS.Core.Loggings;
+
+namespace TTSS.Core.gRPC.Interceptors;
+
+internal sealed class GrpcExceptionParameter<TRequest> where TRequest : class
+{
+    #region Fields
+
+    private string? _requestJson;
+
+    #endregion
+
+    #region Properties
+
+    public string RequestJson => _requestJson ??= JsonSerializer.Serialize(Request);
+    public TRequest Request { get; }
+    public IActivity? Activity { get; }
+    public string CorrelationId { get; }
+    public ServerCallContext Context { get; }
+
+    #endregion
+
+    #region Constructors
+
+    public GrpcExceptionParameter(TRequest request, ServerCallContext context, IActivity? activity)
+    {
+        Request = request ?? throw new ArgumentNullException(nameof(request));
+        Activity = activity ?? throw new ArgumentNullException(nameof(activity));
+        Context = context ?? throw new ArgumentNullException(nameof(context));
+        CorrelationId = activity?.RootId ?? activity?.ParentId ?? activity?.CurrentId ?? "Unknown";
+    }
+
+    #endregion
+}
