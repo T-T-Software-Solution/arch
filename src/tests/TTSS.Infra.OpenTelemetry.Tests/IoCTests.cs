110b3ac2bedeacf6275d91db8e72caae1fee3709
Au <au.perspectives@gmail.com>
2024-01-09 19:27:12 +0700

    Migrate Infra.OpenTelemetry and its test project.

---
+++ src/tests/TTSS.Infra.OpenTelemetry.Tests/IoCTests.cs
@@ -0,0 +1,18 @@
+﻿using AutoFixture;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TTSS.Infra.Loggings.OpenTelemetry;
+
+namespace TTSS.Infra.Infra.Loggings;
+
+public class IoCTests : CommonTestCases
+{
+    public override string ExpectedLogCategoryName => nameof(IoCTests);
+
+    protected override void RegisterServices(IServiceCollection services)
+    {
+        var config = Fixture.Create<TTSS.Infra.Loggings.OpenTelemetry.Models.OTelConfiguration>();
+        services.RegisterOpenTelemetry(config);
+        services.AddTransient<ILoggerFactory>(pvd => LoggerFactoryMock.Object);
+    }
+}
