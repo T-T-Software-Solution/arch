27e0f587a5a4ad73b31ad9421f8bf0dff31a0091
Au <au.perspectives@gmail.com>
2024-09-19 19:46:06 +0700

    Add RemotePublicationHandlerAsync.

---
+++ src/TTSS.Core.Remoting/Core/Messaging/Handlers/RemotePublicationHandlerAsync.cs
@@ -0,0 +1,41 @@
+﻿namespace TTSS.Core.Messaging.Handlers;
+
+/// <summary>
+/// Base class for an asynchronous remote publication handler.
+/// </summary>
+/// <typeparam name="TPublication">The publication type</typeparam>
+public abstract class RemotePublicationHandlerAsync<TPublication> : RemoteRequestHandlerAsync,
+    MassTransit.IConsumer<TPublication>
+    where TPublication : class, IRemotePublication
+{
+    #region Properties
+
+    /// <summary>
+    /// Consumer context.
+    /// </summary>
+    protected MassTransit.ConsumeContext<TPublication> RemoteContext { get; private set; } = null!;
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Defines a class that is a consumer of a message. The message is wrapped in an IConsumeContext
+    /// interface to allow access to details surrounding the inbound message, including headers.
+    /// </summary>
+    Task MassTransit.IConsumer<TPublication>.Consume(MassTransit.ConsumeContext<TPublication> context)
+    {
+        RemoteContext = context;
+        return HandleAsync(context.Message, context.CancellationToken);
+    }
+
+    /// <summary>
+    /// Handles a request.
+    /// </summary>
+    /// <param name="request">The request</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>Response from the request</returns>
+    public abstract Task HandleAsync(TPublication request, CancellationToken cancellationToken = default);
+
+    #endregion
+}
